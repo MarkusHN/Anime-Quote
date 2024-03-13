6ba1df4e416c5a518828b6ccf72a27b91798ccb5
MarkusHN <145451050+MarkusHN@users.noreply.github.com>
2024-03-13 11:15:49 +0100

    push

---
+++ js/anieapi.js
@@ -0,0 +1,28 @@
+async function getQuote() {
+    let quote;
+    let character;
+    let title;
+    const data = await fetch("https://animechan.xyz/api/random");
+    const json = await data.json();
+    quote = json.quote;
+    character = json.character;
+    title = json.anime
+
+    if(quote){
+        document.getElementById("quote").innerText = "''" + quote + "''";
+    }
+
+    if(character){
+        document.getElementById("character").innerText = "By: " + character;
+    }
+
+    if(title){
+        document.getElementById("title").innerText = "From: " + title
+    }
+}
+getQuote();
+
+function newQuote() {
+    location.reload();
+}
+
